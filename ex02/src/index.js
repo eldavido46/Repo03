c592d611299ac0db22e86694de5d777663774b52
eldavido46 <90601320+eldavido46@users.noreply.github.com>
2021-09-15 22:06:40 +0200

    Add files via upload

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,1 +1,17 @@
+function main () {
+    // console.log('Hello world!');
 
+}
+
+main ();
+
+module.exports = main;
+// This is single line JavaScript comment
+
+
+/* document.write("This is a multi-line comment!");
+document.write("This is a multi-line comment!");
+document.write("This is a multi-line comment!");
+document.write("This is a multi-line comment!");
+document.write("This is a multi-line comment!");
+ */
