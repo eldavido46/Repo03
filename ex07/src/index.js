48dc0dfe01dd72d323af0d7baa8f1d1987bcc3e7
eldavido46 <90601320+eldavido46@users.noreply.github.com>
2021-09-16 09:35:44 +0200

    Add files via upload

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,1 +1,23 @@
+function main () {
+   // Variable declerations
+   var StUdlyCapVar;
+   var properCamelCase;
+   var TitleCaseOver;
+
+   // Variable assignments
+   StUdlyCapVar = 10;
+   properCamelCase = "A String";
+   TitleCaseOver = 9000;
+
+
+return {
+    StUdlyCapVar,
+    properCamelCase,
+    TitleCaseOver
+}
+}
+
+console.log(main());
+
+module.exports = main;
 
