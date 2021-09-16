3d6ad727a83c19a9109e58629f37d49d179aba2b
eldavido46 <90601320+eldavido46@users.noreply.github.com>
2021-09-16 09:23:59 +0200

    Add files via upload

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,1 +1,18 @@
+function main () {
+    var x;
+    var y;
+    var z;
+
+    x = x + 2;
+    y = y + 4;
+    z = z + "John!";
+
+
+return [x, y, z]
+
+}
+
+console.log(main());
+
+module.exports = main;
 
